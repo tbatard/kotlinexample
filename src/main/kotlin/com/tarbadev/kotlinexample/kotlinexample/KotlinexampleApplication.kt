89799664ee5b297029c3926a8943a59ac804ba4a
Tony Batard <tbatard@pivotal.io>
2018-09-21 13:55:34 -0700

    Initial kotlin project setup

---
+++ src/main/kotlin/com/tarbadev/kotlinexample/kotlinexample/KotlinexampleApplication.kt
@@ -0,0 +1,11 @@
+package com.tarbadev.kotlinexample.kotlinexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinexampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinexampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/tarbadev/kotlinexample/kotlinexample/KotlinexampleApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.tarbadev.kotlinexample.kotlinexample
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinexampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
